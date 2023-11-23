0a368591ce715c4b514ddd9f56f314c1cdf541c5
ChrisGarozzo <garozzochristian@gmail.com>
2023-11-23 18:51:31 +0100

    bug thumbnails slide next on js

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -1,5 +1,9 @@
 const arrayImage = ["img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp"];
-console.log(arrayImage);
+
+const thumbnails = document.getElementsByClassName("thumb-item");
+// console.log(thumbnails[0]);
+// thumbnails[0].style.filter = "none";
+
 
 let img = document.getElementById("img");
 img.src = arrayImage[0];
@@ -10,63 +14,25 @@ btn_next.addEventListener("click",
     function () {
         index = (index + 1) % arrayImage.length;
         img.src = arrayImage[index];
+        thumbnails[index - 1].classList.remove("filter-none");
+        thumbnails[index].classList.add("filter-none");
+        if (index === arrayImage.length - 1) {
+            thumbnails[index].classList.remove("filter-none");
+            index = 0;
+            thumbnails[index].classList.add("filter-none");
+        }
     }
 )
 
 const btn_previous = document.getElementById("previous");
 btn_previous.addEventListener("click",
     function () {
-        if(index > 0){
+        if (index > 0) {
             index--;
         }
-        else{
+        else {
             index = arrayImage.length - 1;
         }
         img.src = arrayImage[index];
     }
 )
-
-
-
-
-
-
-
-
-// const item_colletion = document.getElementsByClassName("item");
-// console.log(item_colletion);
-
-
-// let index = 0;
-// const btn_next = document.getElementById("next");
-// btn_next.addEventListener("click",
-//     function () {
-//         btn_previous.style.display = "block";
-//         if (index < item_colletion.length - 1) {
-//             item_colletion[index].classList.remove("active");
-//             index++;
-//             item_colletion[index].classList.add("active");
-//             if (index === item_colletion.length - 1) {
-//                 btn_next.style.display = "none";
-//                 index = 0
-//             }
-//         }
-//     }
-// )
-
-// let i = item_colletion.length - 1;
-// const btn_previous = document.getElementById("previous");
-// btn_previous.addEventListener("click",
-//     function () {
-//         btn_next.style.display = "block";
-//         if (i >= 0) {
-//             item_colletion[i].classList.remove("active");
-//             i--;
-//             item_colletion[i].classList.add("active");
-//             if (i === 0) {
-//                 btn_previous.style.display = "none";
-//                 i = item_colletion.length - 1;
-//             }
-//         }
-//     }
-// )
