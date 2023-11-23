445b74c021ff123e01b50db4baf7936bac8dfc45
ChrisGarozzo <garozzochristian@gmail.com>
2023-11-23 16:29:32 +0100

    fix bug switch slide on btn(netx & previous)

--- js/main.js
+++ js/main.js
@@ -6,12 +6,14 @@ let index = 0;
 const btn_next = document.getElementById("next");
 btn_next.addEventListener("click",
     function () {
+        btn_previous.style.display = "block";
         if (index < item_colletion.length - 1) {
             item_colletion[index].classList.remove("active");
             index++;
             item_colletion[index].classList.add("active");
             if (index === item_colletion.length - 1) {
                 btn_next.style.display = "none";
+                index = 0
             }
         }
     }
@@ -21,12 +23,14 @@ let i = item_colletion.length - 1;
 const btn_previous = document.getElementById("previous");
 btn_previous.addEventListener("click",
     function () {
+        btn_next.style.display = "block";
         if (i >= 0) {
             item_colletion[i].classList.remove("active");
             i--;
             item_colletion[i].classList.add("active");
             if (i === 0) {
                 btn_previous.style.display = "none";
+                i = item_colletion.length - 1;
             }
         }
     }
